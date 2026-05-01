63bb8cbb72ed73d0d5eaf672ab4f63a27c90a775
abhishek-jha-24 <abhi2004jha@gmail.com>
2026-05-01 00:06:02 -0700

    refactor: Export voting components from debate module
    - Export VotingAlgorithm, VotingMode, AgentVote, VotingResult
    - Export KeyUpdateManager, KeyUpdateEvent, VotingTrigger
    - Enable clean imports: from arguenet.debate import VotingAlgorithm
    - Simplifies integration for downstream code

--- arguenet/debate/__init__.py
+++ arguenet/debate/__init__.py
@@ -1,1 +1,22 @@
 """Arguenet debate orchestration."""
+
+from .voting import VotingAlgorithm, VotingMode, AgentVote, VotingResult
+from .key_updates import KeyUpdateManager, KeyUpdateEvent, VotingTrigger
+from .consensus import build_consensus
+from .round import run_round, score_round
+from .termination import should_terminate
+
+__all__ = [
+    "VotingAlgorithm",
+    "VotingMode",
+    "AgentVote",
+    "VotingResult",
+    "KeyUpdateManager",
+    "KeyUpdateEvent",
+    "VotingTrigger",
+    "build_consensus",
+    "run_round",
+    "score_round",
+    "should_terminate",
+]
+
